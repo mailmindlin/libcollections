cde038e3b04e1d05d84e15369b1789b6c94de785
mailmindlin <mailmindlin@users.noreply.github.com>
2016-12-15 11:30:47 -0500

    Update queue.h

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -6,16 +6,19 @@ typedef int (Comparator)(void* a, void* b);
 typedef void (Cleaner) (void* value);
 
 typedef enum {
-	UnknownType,
-	FibonacciHeap,
-	PairingHeap,
+	UnknownCollectionType,
+	FibonacciQueue,
+	PairingQueue,
+	BinaryQueue,
+	KeyedFibonacciQueue,
+	KeyedPairingQueue,
 	BinaryHeap,
 	NaryHeap,
 	BinomialHeap,
 	LinkedQueue,
 	BrodalQueue,
 	StrictFibonacciHeap
-} RelativePriorityQueueType;
+} CollectionType;
 
 typedef struct {
 	bool (*push) (RelativePriorityQueue* queue, void* value);
@@ -24,10 +27,22 @@ typedef struct {
 	bool (*isEmpty) (RelativePriorityQueue* queue);
 	void (*clear) (RelativePriorityQueue* queue, Cleaner* cleaner);
 	void (*release) (RelativePriorityQueue* queue, Cleaner* cleaner);
-	RelativePriorityQueueType type;
+	CollectionType type;
+	unsigned char[sizeof(void*) * 4] priv;//Implementation-specific data
+} Queue;
+
+typedef struct {
+	bool (*push) (KeyedPriorityQueue* queue, unsigned int key, void* value);
+	void* (*peek) (KeyedPriorityQueue* queue);
+	unsigned int (*peekKey) (KeyedPriorityQueue* queue);
+	void* (*pop) (KeyedPriorityQueue* queue);
+	bool (*isEmpty) (KeyedPriorityQueue* queue);
+	void (*clear) (KeyedPriorityQueue* queue, Cleaner* cleaner);
+	void (*release) (KeyedPriorityQueue* queue, Cleaner* cleaner);
+	CollectionType type;
 	Comparator* comparator;
 	unsigned char[sizeof(void*) * 4] priv;//Implementation-specific data
-} RelativePriorityQueue;
+} KeyedPriorityQueue;
 
 LIBCOLLECTIONS_PUBLIC RelativePriorityQueue* InitRelativePriorityQueue(RelativePriorityQueue* queue, RelativePriorityQueueType type, Comparator* comparator);
 
