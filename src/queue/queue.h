d8c14eecb22329459f4fae7837ab7a6e418942e3
mailmindlin <mailmindlin@users.noreply.github.com>
2016-12-19 11:48:31 -0500

    Fixed typedefs

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -6,18 +6,26 @@
 typedef int (Comparator)(void* a, void* b);
 typedef void (Cleaner) (void* value);
 
-typedef struct {
-	bool (*push) (RelativePriorityQueue* queue, void* value);
-	void* (*peek) (RelativePriorityQueue* queue);
-	void* (*pop) (RelativePriorityQueue* queue);
-	bool (*isEmpty) (RelativePriorityQueue* queue);
-	void (*clear) (RelativePriorityQueue* queue, Cleaner* cleaner);
-	void (*release) (RelativePriorityQueue* queue, Cleaner* cleaner);
+struct Queue_s;
+struct RelativePriorityQueue_s;
+struct KeyedPriorityQueue_s;
+
+typedef struct Queue_s Queue;
+typedef struct RelativePriorityQueue_s RelativePriorityQueue;
+typedef struct KeyedPriorityQueue_s KeyedPriorityQueue;
+
+struct Queue_s {
+	bool (*push) (Queue* queue, void* value);
+	void* (*peek) (Queue* queue);
+	void* (*pop) (Queue* queue);
+	bool (*isEmpty) (Queue* queue);
+	void (*clear) (Queue* queue, Cleaner* cleaner);
+	void (*release) (Queue* queue, Cleaner* cleaner);
 	CollectionType type;
 	unsigned char[sizeof(void*) * 4] priv;//Implementation-specific data
-} Queue;
+};
 
-typedef struct {
+struct RelativePriorityQueue_s {
 	bool (*push) (RelativePriorityQueue* queue, void* value);
 	void* (*peek) (RelativePriorityQueue* queue);
 	void* (*pop) (RelativePriorityQueue* queue);
@@ -27,9 +35,9 @@ typedef struct {
 	CollectionType type;
 	Comparator* comparator;
 	unsigned char[sizeof(void*) * 4] priv;//Implementation-specific data
-} RelativePriorityQueue;
+};
 
-typedef struct {
+struct KeyedPriorityQueue_s {
 	bool (*push) (KeyedPriorityQueue* queue, unsigned int key, void* value);
 	void* (*peek) (KeyedPriorityQueue* queue);
 	unsigned int (*peekKey) (KeyedPriorityQueue* queue);
@@ -39,7 +47,7 @@ typedef struct {
 	void (*release) (KeyedPriorityQueue* queue, Cleaner* cleaner);
 	CollectionType type;
 	unsigned char[sizeof(void*) * 4] priv;//Implementation-specific data
-} KeyedPriorityQueue;
+};
 
 LIBCOLLECTIONS_PUBLIC Queue* InitQueue(Queue* queue, CollectionType type);
 LIBCOLLECTIONS_PUBLIC RelativePriorityQueue* InitRelativePriorityQueue(RelativePriorityQueue* queue, CollectionType type, Comparator* comparator);
