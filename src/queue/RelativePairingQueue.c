714d4f75455c97d55bb9434013f917227eec1638
mailmindlin <mailmindlin@users.noreply.github.com>
2016-12-19 13:39:10 -0500

    Updated PairingRPQ

--- src/queue/RelativePairingQueue.c
+++ src/queue/RelativePairingQueue.c
@@ -5,16 +5,16 @@
 
 //Pairing heap implementation
 //see https://users.cs.fiu.edu/~weiss/dsaa_c++/code/PairingHeap.cpp, https://www.cs.cmu.edu/~sleator/papers/pairing-heaps.pdf
-struct PriorityQueueNode_s;
-typedef struct PriorityQueueNode_s PriorityQueueNode;
-struct PriorityQueueNode_s {
-	PriorityQueueNode* parent;
-	PriorityQueueNode* child;
-	PriorityQueueNode* sibiling;
+struct PairingRPQNode_s;
+typedef struct PairingRPQNode_s PairingRPQNode;
+struct PairingRPQNode_s {
+	PairingRPQNode* parent;
+	PairingRPQNode* child;
+	PairingRPQNode* sibiling;
 	void *value;
 };
 
-static PriorityQueueNode* doMerge(PriorityQueueNode* treeA, PriorityQueueNode* treeB, Comparator* comparator) {
+static PairingRPQNode* doMerge(PairingRPQNode* treeA, PairingRPQNode* treeB, Comparator* comparator) {
 	if (treeA == NULL)
 		return treeB;
 	if (treeB == NULL)
@@ -22,42 +22,34 @@ static PriorityQueueNode* doMerge(PriorityQueueNode* treeA, PriorityQueueNode* t
 	
 	if (comparator(treeA->value, treeB->value) > 0) {
 		//Attach treeA as child of treeB
-		treeB->parent = treeA->parent;
-		treeA->parent = treeB;
 		treeA->sibiling = treeB->child;
-		//?
-		if (treeA->sibiling != NULL)
-			treeA->sibiling->parent = treeA;
 		treeB->child = treeA;
 		return treeB;
 	} else {
 		//Attach treeB as child of treeA
-		treeB->parent = treeA;
 		treeA->sibiling = treeB->sibiling;
-		if (treeA->sibiling != NULL)
-			treeA->sibiling->parent = treeA;
 		treeB->sibiling = treeA->child;
 		treeA->child = treeB;
 		return treeA;
 	}
 }
 
-static PriorityQueueNode* combineSibilings(PriorityQueueNode* firstSibiling, Comparator* comparator) {
+static PairingRPQNode* combineSibilings(PairingRPQNode* firstSibiling, Comparator* comparator) {
 	if (firstSibiling == NULL || firstSibiling->sibiling == NULL)
 		return firstSibiling;
 	size_t treeArrayCapacity = 8;
-	PriorityQueueNode** treeArray = malloc(8 * sizeof(PriorityQueueNode*));
+	PairingRPQNode** treeArray = malloc(8 * sizeof(PairingRPQNode*));
 	if (!treeArray) {
 		//OOM error
 		return NULL;
 	}
 	size_t numPairs = 0;
-	PriorityQueueNode* currentNode = firstSibiling;
+	PairingRPQNode* currentNode = firstSibiling;
 	//Fill treeArray with pairs of nodes, going left-to-right.
 	do {
 		if (numPairs >= treeArrayCapacity) {
 			size_t newTAC = treeArrayCapacity * 2 / 3 + 1;
-			PriorityQueueNode** newTreeArray = realloc(treeArray, newTAC * sizeof(PriorityQueueNode*));
+			PairingRPQNode** newTreeArray = realloc(treeArray, newTAC * sizeof(PairingRPQNode*));
 			if (newTreeArray == NULL) {
 				//OOM error
 				free(treeArray);
@@ -66,8 +58,8 @@ static PriorityQueueNode* combineSibilings(PriorityQueueNode* firstSibiling, Com
 			treeArrayCapacity = newTAC;
 			treeArray = newTreeArray;
 		}
-		PriorityQueueNode* sibilingA = currentNode;
-		PriorityQueueNode* sibilingB = sibilingA->sibiling;
+		PairingRPQNode* sibilingA = currentNode;
+		PairingRPQNode* sibilingB = sibilingA->sibiling;
 		sibilingA->sibiling = NULL;
 		if (sibilingB == NULL) {
 			//Last sibiling (odd #, no other to pair it with)
@@ -80,7 +72,7 @@ static PriorityQueueNode* combineSibilings(PriorityQueueNode* firstSibiling, Com
 	} while (currentNode != NULL);
 	
 	//Merge/accumulate remaining pairs right-to-left
-	PriorityQueueNode* lastPair = treeArray[numPairs - 1];
+	PairingRPQNode* lastPair = treeArray[numPairs - 1];
 	numPairs--;
 	for(;numPairs > 0; numPairs--)
 		lastPair = doMerge(treeArray[numPairs - 1], lastPair, comparator);
@@ -88,11 +80,11 @@ static PriorityQueueNode* combineSibilings(PriorityQueueNode* firstSibiling, Com
 	return lastPair;
 }
 
-static void doReleaseNode(PriorityQueueNode* node, Cleaner* cleaner) {
-	PriorityQueueNode* current = node;
+static void doReleaseNode(PairingRPQNode* node, Cleaner* cleaner) {
+	PairingRPQNode* current = node;
 	while (current != NULL) {
-		PriorityQueueNode* sibiling = current->sibiling;
-		PriorityQueueNode* child = current->child;
+		PairingRPQNode* sibiling = current->sibiling;
+		PairingRPQNode* child = current->child;
 		cleaner(current->value);
 		free(current);
 		current = sibiling;
@@ -109,37 +101,34 @@ void* PairingRPQ_peek(RelativePriorityQueue* queue) {
 	if (queue->priv[0] == NULL)
 		//Underflow
 		return NULL;
-	return ((PriorityQueueNode*)queue->priv[0])->value;
+	return queue->priv.pairingRoot->value;
 }
 
 void* PairingRPQ_pop(RelativePriorityQueue* queue) {
-	PriorityQueueNode* oldRoot = ((PriorityQueueNode*)queue->priv[0]);
+	PairingRPQNode* oldRoot = queue->priv.pairingRoot;
 	if (oldRoot == NULL)
 		//Underflow
 		return NULL;
 	void* result = oldRoot->value;
-	PriorityQueueNode* firstChild = oldRoot->child;
+	PairingRPQNode* firstChild = oldRoot->child;
 	free(oldRoot);
-	queue->priv[0] = (void*) combineSibilings(firstChild, queue->comparator);
+	queue->priv.pairingRoot = (void*) combineSibilings(firstChild, queue->comparator);
 	return result;
 }
 
 bool PairingRPQ_push(RelativePriorityQueue* queue, void* value) {
-	PriorityQueueNode* node = malloc(sizeof(PriorityQueueNode));
+	PairingRPQNode* node = malloc(sizeof(PairingRPQNode));
 	if (node == NULL)
 		return false;
-	node->parent = NULL;
 	node->child = NULL;
 	node->sibiling = NULL;
 	node->value = value;
-	PriorityQueueNode* root = ((PriorityQueueNode*)queue->priv[0]);
-	root = doMerge(root, node, queue->comparator);
-	queue->priv[0] = (void*) root;
+	queue->priv.pairingRoot = doMerge(queue->priv.pairingRoot, node, queue->comparator);
 	return true;
 }
 
 void PairingRPQ_clear(RelativePriorityQueue* queue, Cleaner* cleaner) {
-	PriorityQueueNode* root = ((PriorityQueueNode*)queue->priv[0]);
+	PairingRPQNode* root = queue->priv.pairingRoot;
 	doReleaseNode(root, cleaner);
 }
 
