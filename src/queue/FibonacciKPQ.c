0ad5d3292b9d82edb1155c7ff8a06c1314b95b30
mailmindlin <liamttlyrox@gmail.com>
2016-12-20 12:03:18 -0500

    Added fibonacci queue shell

---
+++ src/queue/FibonacciKPQ.c
@@ -0,0 +1,33 @@
+#include <stddef.h>	//For NULL
+#include <stdlib.h>	//For malloc, free
+#include <stdbool.h>
+#include "queue.h"
+#include "queue-priv.h"
+
+LIBCOLLECTIONS_LOCAL bool  FibonacciKPQ_push(KeyedPriorityQueue* queue, unsigned int key, void* value) {
+	return false;
+}
+
+LIBCOLLECTIONS_LOCAL void* FibonacciKPQ_peek(KeyedPriorityQueue* queue) {
+	return NULL;
+}
+
+LIBCOLLECTIONS_LOCAL unsigned int FibonacciKPQ_peekKey(KeyedPriorityQueue* queue) {
+	return 0;
+}
+
+LIBCOLLECTIONS_LOCAL void* FibonacciKPQ_pop (KeyedPriorityQueue* queue) {
+	return NULL;
+}
+
+LIBCOLLECTIONS_LOCAL bool  FibonacciKPQ_empty(KeyedPriorityQueue* queue) {
+	return queue->fibonacciKPQData.root == NULL;
+}
+
+LIBCOLLECTIONS_LOCAL void  FibonacciKPQ_clear(KeyedPriorityQueue* queue, Cleaner* cleaner) {
+	
+}
+
+LIBCOLLECTIONS_LOCAL void  FibonacciKPQ_release(KeyedPriorityQueue* queue, Cleaner* cleaner) {
+	
+}
