7445be194d2485f66494fd1bc1d3cf538a683e8a
mailmindlin <liamttlyrox@gmail.com>
2017-01-04 11:04:03 -0500

    Move Comparator, Cleaner to collections.h

--- src/queue/queue.h
+++ src/queue/queue.h
@@ -4,21 +4,6 @@
 #ifndef __LIBCOLLECTIONS_QUEUE_H
 #define __LIBCOLLECTIONS_QUEUE_H
 
-/**
- * A comparison function that is able to produce a total ordering of a set of elements.
- * Care should be given such that the ordering of all elements is consitent (for example,
- * if cmp(a,b) > 0, cmp(b,c) > 0, then cmp(a,c) must > 0).
- * Returns a positive number if a > b
- * Returns 0 if a is equivalent to b
- * Returns a negative number if a < b
- */
-typedef int (Comparator)(void* a, void* b);
-/**
- * A method that safely releases the passed element.
- * Intentionally the same signature as free().
- */
-typedef void (Cleaner) (void* value);
-
 struct PairingRPQNode;
 struct PairingKPQNode;
 struct FibonacciRPQNode;
