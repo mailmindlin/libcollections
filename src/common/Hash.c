72c6ea0f9a4eafd7085ffe0479e5b0dedfbc2a71
mailmindlin <liamttlyrox@gmail.com>
2017-01-18 22:39:30 -0500

    Add some hash functions

---
+++ src/common/Hash.c
@@ -0,0 +1,65 @@
+#include "Hash.h"
+#include "xxhash.h"
+#include <stdlib.h>//For malloc, free
+
+static void release_noop(void* p) {
+	((void)p);
+}
+
+static unsigned long Identity_apply(void* p, void* v) {
+	((void)p);
+	return (unsigned long) v;
+}
+
+IntFunction* Identity_init(IntFunction* fn) {
+	fn->apply = &Identity_apply;
+	fn->release = &release_noop;
+	fn->priv = NULL;
+	return fn;
+}
+
+static unsigned long StringHash_apply(void* p, void* v) {
+	((void)p);
+	if (v == NULL)
+		return 0;
+	char c;
+	unsigned long result = 0;
+	for (char* str = (char*) v; c = *str; str++)
+		result = result * 31 + c;
+	return result;
+}
+
+IntFunction* StringHash_init(IntFunction* fn) {
+	fn->apply = &StringHash_apply;
+	fn->release = &release_noop;
+	fn->priv = NULL;
+	return fn;
+}
+
+struct FixedXXH_data {
+	unsigned long seed;
+	size_t length;
+};
+
+static unsigned long FixedXXH_apply(void* priv, void* value) {
+	if (value == NULL)
+		return 0;
+	struct FixedXXH_data* data = (struct FixedXXH_data*) priv;
+	//XXH64 is over 2x as fast as XXH32 on 64-bit machines (as could be expected), but is MUCH slower on 32-bit computers (1/3x).
+	#if sizeof(unsigned long) >= 8
+		return (unsigned long) XXH64(value, data->length, (unsigned long long) data->seed);
+	#else
+		return (unsigned long) XXH32(value, data->length, (unsigned int) data->seed);
+	#endif
+}
+
+IntFunction* FixedXXH_init(IntFunction* fn, unsigned long seed, size_t structSize) {
+	if ((fn->priv = malloc(sizeof(FixedXXH_data))) == NULL)
+		return NULL;
+	((struct FixedXXH_data*)fn->priv)->seed = seed;
+	((struct FixedXXH_data*)fn->priv)->length = structSize;
+	fn->apply = &FixedXXH_apply;
+	fn->release = &free;
+	fn->priv = NULL;
+	return fn;
+}
---
+++ src/common/Hash.h
@@ -0,0 +1,14 @@
+/**
+ * Usefull hash functions
+ */
+#ifndef __LIBCOLLECTIONS_HASH_H
+#define __LIBCOLLECTIONS_HASH_H
+
+#include "../locality.h"
+#include "Functions.h"
+
+LIBCOLLECTIONS_PUBLIC IntFunction* Identity_init(IntFunction* fn);
+LIBCOLLECTIONS_PUBLIC IntFunction* StringHash_init(IntFunction* fn);
+LIBCOLLECTIONS_PUBLIC IntFunction* FixedXXH_init(IntFunction* fn, size_t structSize);
+
+#endif //__LIBCOLLECTIONS_HASH_H
