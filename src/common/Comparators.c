3c340dc44bed36efd7385385d59e4b518d57ead8
mailmindlin <liamttlyrox@gmail.com>
2017-01-18 22:39:43 -0500

    Add some comparators

---
+++ src/common/Comparators.c
@@ -0,0 +1,61 @@
+#include "Comparators.h"
+#include <string.h> //For strcmp, memcmp, malloc, free
+
+static void release_noop(void* p) {
+	((void)p);
+}
+
+static int PointerComparator_apply(void* p, const void* a, const void* b) {
+	unsigned long aVal = (unsigned long) a;
+	unsigned long bVal = (unsigned long) b;
+	if (aVal > bVal)
+		return 1;
+	if (aVal == bVal)
+		return 0;
+	//assert(aVal < bVal)
+	return -1;
+}
+
+Comparator* PointerComparator_init(Comparator* comparator) {
+	comparator->apply = &PointerComparator_apply;
+	comparator->release = &release_noop;
+	comparator->priv = NULL;
+	return comparator;
+}
+
+static int FixedLengthValueComparator_init(void* p, const void* a, const void* b) {
+	if (a == b)
+		return 0;
+	if (a == NULL)
+		return -1;
+	if (b == NULL)
+		return 1;
+	size_t length = *((size_t*)p);
+	return memcmp(a, b, length);
+}
+
+Comparator* FixedLengthValueComparator_init(Comparator* comparator, size_t length) {
+	if ((comparator->priv = malloc(sizeof(size_t))) == NULL)
+		return NULL;
+	*((size_t*)comparator->priv) = length;
+	comparator->apply = &PointerComparator_apply;
+	comparator->release = &free;
+	return comparator;
+}
+
+static int StringComparator_apply(void* p, const void* a, const void* b) {
+	if (a == b)
+		return 0;
+	if (a == NULL)
+		return -1;
+	if (b == NULL)
+		return 1;
+	return strcmp((const char*)a, (const char*)b);
+}
+
+Comparator* StringComparator_init(Comparator* comparator) {
+	comparator->apply = &StringComparator_apply;
+	comparator->release = &release_noop;
+	comparator->priv = NULL;
+	return comparator;
+}
---
+++ src/common/Comparators.h
@@ -0,0 +1,12 @@
+#ifndef __LIBCOLLECTIONS_COMMON_COMPARATORS_H
+#define __LIBCOLLECTIONS_COMMON_COMPARATORS_H
+
+#include "Functions.h"
+#include "../locality.h"
+
+LIBCOLLECTIONS_PUBLIC Comparator* PointerComparator_init(Comparator* comparator);
+
+LIBCOLLECTIONS_PUBLIC Comparator* FixedLengthValueComparator_init(Comparator* comparator, size_t length);
+
+LIBCOLLECTIONS_PUBLIC Comparator* StringComparator_init(Comparator* comparator);
+#endif //__LIBCOLLECTIONS_COMMON_COMPARATORS_H
